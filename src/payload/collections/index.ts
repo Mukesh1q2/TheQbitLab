9ac9aa2eef56257609cd78c02fdb954f8bce841e
Mukesh1q2 <mukesh1q2@gmail.com>
2025-12-21 12:18:49 +0530

    Fix build: Remove unused payload cms directory

--- src/payload/collections/index.ts
+++
@@ -1,9 +0,0 @@
-// Payload Collections Export
-export { Users } from './Users'
-export { Media } from './Media'
-export { Posts } from './Posts'
-export { Projects } from './Projects'
-export { Pages } from './Pages'
-export { Team } from './Team'
-export { Services } from './Services'
-export { Testimonials } from './Testimonials'
